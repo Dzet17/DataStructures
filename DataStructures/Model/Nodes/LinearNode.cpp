63f74ce61d4e5e6478181faf5f6d0f592a3d4a24
Dylan Zettler <dylanzettler@gmail.com>
2018-02-08 09:30:09 -0700

    LinearNode.hpp is done

--- DataStructures/Model/Nodes/LinearNode.cpp
+++ DataStructures/Model/Nodes/LinearNode.cpp
@@ -6,46 +6,4 @@
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
-#include "Node.hpp"
-
-template <class Type>
-class LinearNode : public Node<Type>
-{
-    //This section is available to the subclasses of LinearCode
-protected:
-    LinearNode<Type> * next;
-public:
-    //Constructors
-    LinearNode();
-    LinearNode(Type data);
-    LinearNode(Type data, LinearNode<Type> * next);
-    //Methods
-    void setNextNode(LinearNode<Type> * next);
-    LinearNode<Type> * getNextNode();
-};
-
-template <class Type>
-LinearNode<Type> :: LinearNode() : Node<Type>()
-{
-    this->next = nullptr;
-}
-
-template <class Type>
-LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data);
-{
-    this->next = next;
-}
-
-//Accessor methods
-
-template <class Type>
-void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer )
-{
-    this -> next = nextNodePointer;
-}
-
-template <class Type>
-LinearNode<Type> * LinearNode<Type> :: getNextNode()
-{
-    return next;
-}
+#include "LinearNode.hpp"
---
+++ DataStructures/Model/Nodes/LinearNode.hpp
@@ -0,0 +1,56 @@
+//
+//  LinearNode.hpp
+//  DataStructures
+//
+//  Created by Zettler, Dylan on 2/8/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinearNode_hpp
+#define LinearNode_hpp
+
+#include "Node.hpp"
+
+template <class Type>
+class LinearNode : public Node<Type>
+{
+    //This section is available to the subclasses of LinearCode
+protected:
+    LinearNode<Type> * next;
+public:
+    //Constructors
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    //Methods
+    void setNextNode(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+};
+
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+//Accessor methods
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer )
+{
+    this -> next = nextNodePointer;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
+
+#endif /* LinearNode_hpp */
