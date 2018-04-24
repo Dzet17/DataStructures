967d671b1686f68a7cfac6fd3f30f3d317e528bf
Dylan Zettler <dylanzettler@gmail.com>
2018-04-24 07:33:34 -0600

    Updated N_AryTreeNode

--- DataStructures/Model/Nodes/N_AryTreeNode.hpp
+++ DataStructures/Model/Nodes/N_AryTreeNode.hpp
@@ -24,18 +24,64 @@ public:
     ~N_AryTreeNode<Type>();
     N_AryTreeNode<Type>(Type data);
     
-    set<N_AryTreeNode<Type>*> getNodes();
+    vector<N_AryTreeNode<Type>*> getNodes();
     N_AryTreeNode<Type> * getRoot();
     int getChildCount();
     
     void setRoot(N_AryTreeNode<Type> * root);
-    void addChild(N_AryTreeNode<Type> * node);
+    void addChild(Type data);
 };
 
 template <class Type>
 N_AryTreeNode<Type> :: N_AryTreeNode()
-{ //unused
-    
+{
+    nodes.clear();
 }
+
+template <class Type>
+N_AryTreeNode<Type> :: N_AryTreeNode(Type data) : Node<Type>(data)
+{
+}
+
+template <class Type>
+N_AryTreeNode<Type> :: ~N_AryTreeNode()
+{
+    for (std::set<N_AryTreeNode<Type> *>::iterator iter = nodes.begin(); iter != nodes.end(0); iter++)
+    {
+        nodes.erase(iter);
+        iter--;
+    }
+}
+
+template <class Type>
+N_AryTreeNode<Type> * N_AryTreeNode<Type> :: getRoot()
+{
+    return root;
+}
+template <class Type>
+set<N_AryTreeNode<Type>*> N_AryTreeNode<Type> :: getNodes()
+{
+    return nodes;
+}
+
+template <class Type>
+int N_AryTreeNode<Type> :: getChildCount()
+{
+    return nodes.size();
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: setRoot(N_AryTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: addChild(Type data)
+{
+    N_AryTreeNode<Type> * node = new N_AryTreeNode<Type>(data);
+    nodes.insert(node);
+}
+
 
 #endif /* N_AryTreeNode_hpp */
